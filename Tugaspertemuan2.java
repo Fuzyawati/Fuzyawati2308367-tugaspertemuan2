b6a170dce119860524e3aa395ab1560127a1147d
Fuzyawati2308367 <fuzyawatinrp@upi.edu>
2025-02-18 23:42:30 +0700

    Add files via upload

---
+++ Tugaspertemuan2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fuzyawati2308367.tugaspertemuan2;
+
+/**
+ *
+ * @author Desa Cibatu
+ */
+public class Tugaspertemuan2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
